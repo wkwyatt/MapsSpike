50fd8b585afe0deaca23ecd1606d6dc20e3468b8
wkwyatt <wkwyatt@gmail.com>
2015-10-17 19:04:38 -0400

    Added Frameworks and setup CLLocationManager
    Added the MapKit and CoreLocation frameworks, added to keys to the
    pList, and set up the delegates for locationManager

--- MapsSpike/ViewController.swift
+++ MapsSpike/ViewController.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+// Make sure to import MapKit and CoreLocation frameworks
+import CoreLocation
 
-class ViewController: UIViewController {
+// Add the location manager delegate to the class to conform to the CLLocationManager
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // Create location manager
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set the delegate to the view
+        locationManager.delegate = self
+        
+        // Request authorization to use location services
+        locationManager.requestWhenInUseAuthorization()
+        
+        // Start updating the location as soon as possible
+        locationManager.startUpdatingLocation()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
+        
+        println("Got location \(newLocation.coordinate.latitude) , \(newLocation.coordinate.longitude)")
+    
+    
     }
 
 
