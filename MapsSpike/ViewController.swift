faba92ce6a11f49321e998be85d8176009d5796c
wkwyatt <wkwyatt@gmail.com>
2015-10-17 21:17:54 -0400

    Enabled user to get directions to the dropped pin
    Added mapView delegate to the view controller

--- MapsSpike/ViewController.swift
+++ MapsSpike/ViewController.swift
@@ -12,13 +12,16 @@ import CoreLocation
 import MapKit
 
 // Add the location manager delegate to the class to conform to the CLLocationManager
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     // Create location manager
     var locationManager = CLLocationManager()
     
     // Save the location of the user in variable instead of constantly updating the location and using unnecessary memory
     var myPosition = CLLocationCoordinate2D(latitude: 37.33233141, longitude: -122)
+    
+    // Destination variable for directions
+    var destination:MKMapItem = MKMapItem()
 
     // Outlets
     @IBOutlet weak var lblLocation: UILabel!
@@ -67,6 +70,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Create drop pin
         let pressedAnnotation = MKPointAnnotation()
         
+        ///////////////////////////
+        // For the directions method
+        
+        // Create a place mark and a map item
+        let placeMark = MKPlacemark(coordinate: localCoord, addressDictionary: nil)
+        
+        // Set the destination for when we get directions
+        destination = MKMapItem(placemark: placeMark)
+        
+        //////////////////////////////
+        
+        
         // Set drop pin attributes and coordinates
         pressedAnnotation.coordinate = localCoord
         pressedAnnotation.title = "My Location"
@@ -79,9 +94,60 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.mapView.addAnnotation(pressedAnnotation)
     }
     
+    @IBAction func showDirections(sender: UIButton) {
+        
+        // Create a MKDirection Request
+        let request = MKDirectionsRequest()
+        
+        // Set the location of the beginning of the route
+        request.setSource(MKMapItem.mapItemForCurrentLocation())
+        
+        // Set the location of the destination
+        request.setDestination(destination)
+        
+        // Set it to only give one route
+        request.requestsAlternateRoutes = false
+        
+        
+        let directions = MKDirections(request: request)
+        
+        directions.calculateDirectionsWithCompletionHandler({(response: MKDirectionsResponse!, error: NSError!) in
+            
+            if error != nil {
+                println("Error")
+            } else {
+                
+                // Remove the current routes that might be displayed
+                var overlays = self.mapView.overlays
+                self.mapView.removeOverlays(overlays)
+                
+                // Get the route from the response
+                for route in response.routes as! [MKRoute] {
+                    
+                    // Display the directions on the map
+                    self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.AboveRoads)
+                    
+                    // Show the steps for the directions
+                    for next in route.steps {
+                        println(next.instructions)
+                    }
+                }
+            }
+        })
+    }
+    
     @IBAction func startGPS(sender: UIButton) {
         
         locationManager.startUpdatingLocation()
+    }
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        
+        // Draw the line used for drawing the directions
+        let draw = MKPolylineRenderer(overlay: overlay)
+        draw.strokeColor = UIColor.greenColor()
+        draw.lineWidth = 3.0
+        return draw
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
